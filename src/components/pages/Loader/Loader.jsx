96bd84cd72482a1c4192eae0ed26e624db58456e
Sasha_Gerzhik <girasik3000@gmail.com>
2026-01-31 15:40:58 +0200

    add Loader

---
+++ src/components/pages/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+import { Box, CircularProgress } from '@mui/material';
+
+const Loader = () => {
+  return (
+    <Box
+      sx={{
+        minHeight: '60vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default Loader;
--- src/components/pages/MoviesList/index.jsx
+++ src/components/pages/MoviesList/index.jsx
@@ -2,13 +2,21 @@ import { Box, Typography } from '@mui/material';
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 
+import Loader from '../Loader/Loader.jsx';
 import { apiUrls } from '../../../constants/api.js';
 
 const MoviesList = () => {
+  const {isLoading} =
   useQuery({
     queryKey: [apiUrls.moviesList],
+    queryFn: async () => {
+      return new Promise((resolve)=>
+      setTimeout(() => resolve([]), 2000))
+    }
   });
-
+if (isLoading){
+  return <Loader/>
+}
   return (
     <Box>
       <Typography variant="h2">Movies list</Typography>
